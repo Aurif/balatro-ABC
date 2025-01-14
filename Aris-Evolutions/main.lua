71ead967664d028df210d7f2470447b675916b53
Aurif <filmosyt@gmail.com>
2025-01-14 23:38:24 +0100

    First evolution card

---
+++ Aris-Evolutions/main.lua
@@ -0,0 +1,18 @@
+--- STEAMODDED HEADER
+--- MOD_NAME: Ari's Evolutions
+--- MOD_ID: ari_evolutions
+--- MOD_AUTHOR: [Aurif]
+--- MOD_DESCRIPTION: More joker evolutions! :3
+--- BADGE_COLOUR: 18cadc
+--- DISPLAY_NAME: Ari's Evolutions
+--- PREFIX: ari_evo
+--- VERSION: 0.0.1
+--- DEPENDENCIES: [Steamodded>=1.0.0~ALPHA-1030f]
+
+----------------------------------------------
+------------MOD CODE -------------------------
+
+SMODS.load_file("jokers/j_chart.lua")()
+
+----------------------------------------------
+------------MOD CODE END----------------------
