099f73b4eac6a20437e4d1d78ea6b6a467002656
Aurif <filmosyt@gmail.com>
2025-03-16 10:58:59 +0100

    ABC - added VARS.Rarity, VARS.Joker:spawn - missing files

---
+++ ABC/base/vars/rarity.lua
@@ -0,0 +1,40 @@
+---
+--- Created by Aurif.
+--- DateTime: 15/03/2025 23:11
+---
+
+---
+--- Base
+---
+
+---@class ABC.VARS.Rarity : ABC.VAR
+---@field value string
+---A variable class representing a rarity.
+---***
+---\@*param* `value` — Key of the rarity.
+---@overload fun(value: string): ABC.VARS.Rarity
+ABC.VARS.Rarity = classABCVar("Rarity")
+
+
+---Returns a random joker from this rarity.
+---***
+---@param seed number Seed to use for rng.
+---@return ABC.VARS.Joker joker Randomly picked joker.
+---***
+---[Example usage](https://github.com/Aurif/balatro-ABC/blob/main/Aris-Random-Stuff/jokers/placeholder_joker.lua)
+function ABC.VARS.Rarity:random_joker(seed)
+    local _pool, _pool_key = get_current_pool_filtered('Joker', self.value)
+    local _joker = pseudorandom_element(_pool, seed)
+    return ABC.VARS.Joker(_joker)
+end
+
+
+---@private
+function ABC.VARS.Rarity:_localize()
+    return {
+        text=localize("k_"..self.value:lower()),
+        extra={
+            colours=G.C.RARITY[self.value]
+        }
+    }
+end
---
+++ Aris-Random-Stuff/jokers/placeholder_joker.lua
@@ -0,0 +1,18 @@
+---
+--- Created by Aurif.
+--- DateTime: 15/03/2025 22:58
+---
+
+ABC.Joker("Placeholder Joker")
+  :description({"When sold or destroyed", "spawn a random {V:1}#rarity#{} joker"})
+  :rarity_common()
+  :variables({
+    rarity = ABC.VARS.Rarity("Uncommon")
+  })
+  :calculate(function(self, card, context, ABCU)
+    ABCU:on_self_destroyed(function ()
+        local joker = ABCU.vars.rarity:random_joker(pseudoseed("Placeholder Joker"))
+        joker:spawn()
+    end)
+  end)
+  :register()
